8ce6990def006d0f51b6bedb6cbc79ac434a69da
Hernan Flores Ramirez <hernanfloresramirez1987@gmail.com>
2026-07-28 14:24:54 -0400

    Add page no found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import ProjectsComponent from './components/pages/projects/projects';
 import SkillsComponent from './components/pages/skills/skills';
 import ContactComponent from './components/pages/contact/contact';
 import ArquitecturaComponent from './components/pages/arquitectura/arquitectura';
+import Error404Component from './components/pages/error404/error404';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -14,5 +15,6 @@ export const routes: Routes = [
     { path: 'arquitectura', component: ArquitecturaComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'skills', component: SkillsComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: Error404Component, pathMatch: 'full' },
 ];
