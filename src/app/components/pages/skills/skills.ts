c180425c2d70fbe3f94979301d868ae0063a2ada
hernan flores ramirez <hernanfloresramirez@MacBook-Pro-de-hernan.local>
2026-08-10 23:16:27 -0400

    add redis

--- src/app/components/pages/skills/skills.ts
+++ src/app/components/pages/skills/skills.ts
@@ -3,10 +3,11 @@ import K8s from './bloques/k8s/k8s';
 import { Docker } from './bloques/docker/docker';
 import { ArgoCd } from "./bloques/argo-cd/argo-cd";
 import { Kafka } from "./bloques/kafka/kafka";
+import { Redis } from './bloques/redis/redis';
 
 @Component({
   selector: 'app-skills',
-  imports: [K8s, Docker, ArgoCd, Kafka],
+  imports: [K8s, Docker, ArgoCd, Kafka, Redis],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
